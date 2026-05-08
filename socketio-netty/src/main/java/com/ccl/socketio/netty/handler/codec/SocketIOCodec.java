f3ec2401c6fa92245c3afe3ee19b51c9f4cd2390
Yujie Wu <coffee377@dingtalk.com>
2026-05-09 00:09:16 +0800

    refactor(socketio-netty): SocketIOCodec 接入 PollingTransport 发送 CONNECT 响应

--- socketio-netty/src/main/java/com/ccl/socketio/netty/handler/codec/SocketIOCodec.java
+++ socketio-netty/src/main/java/com/ccl/socketio/netty/handler/codec/SocketIOCodec.java
@@ -1,9 +1,12 @@
 package com.ccl.socketio.netty.handler.codec;
 
+import com.ccl.engineio.core.protocol.EngineIOPacket;
 import com.ccl.engineio.netty.handler.ChannelAttributes;
+import com.ccl.engineio.netty.transport.PollingTransport;
 import com.ccl.socketio.core.codec.SocketEncoder;
 import com.ccl.socketio.core.codec.impl.SocketIOEncoder;
 import com.ccl.socketio.core.protocol.SocketPacket;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageCodec;
@@ -25,18 +28,33 @@ public class SocketIOCodec extends MessageToMessageCodec<SocketPacket<?>, Socket
 
     @Override
     protected void encode(ChannelHandlerContext ctx, SocketPacket<?> msg, List<Object> out) throws Exception {
-        log.debug("out: {}", msg);
-//        EngineIOPacket<?> engineIOPacket =
-//        socketEncoder
+        if (log.isDebugEnabled()) {
+            log.debug("OUT[{}] {}", msg.getType(), msg);
+        }
+        if (SocketPacket.Type.CONNECT.equals(msg.getType())) {
+            ChannelHandlerContext context = ctx.pipeline().context(PollingTransport.class);
+            if (context != null) {
+                PollingTransport polling = (PollingTransport) context.handler();
+                log.error("{}", context);
+                String data = encoder.encode(msg);
+                EngineIOPacket<String> packet = EngineIOPacket.builder().data(data).build();
+                log.warn("{}", packet);
+                String sid = ctx.channel().attr(ChannelAttributes.SESSION_ID).get();
+                polling.sendMessage(sid, packet);
+            }
+//        out.add(packet);
+        }
+
     }
 
     @Override
     protected void decode(ChannelHandlerContext ctx, SocketPacket<?> msg, List<Object> out) throws Exception {
         SocketPacket.Type type = msg.getType();
-        String sid = ctx.channel().attr(ChannelAttributes.SESSION_ID).get();
         if (log.isDebugEnabled()) {
-            log.debug("Received {} packet from session {}", type, sid);
+            log.debug("IN[{}] {}", type, msg);
         }
+
+        String sid = ctx.channel().attr(ChannelAttributes.SESSION_ID).get();
 
         if (type == SocketPacket.Type.CONNECT) {
             HashMap<String, Object> data = new HashMap<String, Object>() {{
